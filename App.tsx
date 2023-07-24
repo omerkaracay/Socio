4c6855233eb326e56ed32f1e398a23314a723888
Ömer Karaçay <omerkaracay@gmail.com>
2023-07-24 10:35:45 +0300

    Router files seperated from App

--- App.tsx
+++ App.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/screens/LoginScreen';
+import Router from './src/components/Router';
 export default function App() {
-  const Stack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="LoginScreen">
-        <Stack.Screen options={{headerShown: false}} name="LoginScreen" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+<Router />
   );
 }
